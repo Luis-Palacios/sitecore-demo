4c17cfb51543d2fe2b910f390aa21d33bc0a4ee1
Luis Palacios <lrpalaciosdev@gmail.com>
2018-11-01 18:24:39 -0600

    finish remove hardcoded strings

---
+++ SitecoreDemo.Web/Utils/SitecoreConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreDemo.Web.Utils
+{
+    public static class SitecoreConstants
+    {
+        public static class RenderingIds
+        {
+            public const string HEADER = "{16E2BCF3-3CD9-4056-8E09-AC9D6BC28289}";
+            public const string FOOTER = "{99185C5D-DFF2-40AA-9898-74BA256D85DC}";
+        }
+
+        public static class PageIds
+        {
+            public const string HOME = "{39DD0DFB-B06C-49CA-AABE-86EE8A8A5F92}";
+            public const string ABOUT = "{9C0F7B24-E476-452C-93C4-73BCAA5150DB}";
+        }
+
+        public static class ContentFolderIds
+        {
+            public const string TEAM = "{BFDEBBA7-91E9-4B73-AE4C-5EEB683FD852}";
+        }
+
+    }
+}
