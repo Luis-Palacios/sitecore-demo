7abfdc50f5632657ebee5082e63eb060f49db7d7
Luis Palacios <lrpalaciosdev@gmail.com>
2018-11-12 17:36:21 -0600

    part 6 complete

---
+++ SitecoreDemo.Web/Models/Page.cs
@@ -0,0 +1,8 @@
+﻿namespace SitecoreDemo.Web.Models
+{
+    public class Page: BaseModel
+    {
+        public virtual string Name { get; set; }
+        public virtual string Url { get; set; }
+    }
+}
---
+++ SitecoreDemo.Web/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreDemo.Web.Models
+{
+    public class Team: BaseModel
+    {
+        [SitecoreQuery("/sitecore/content/Team/*")]
+        public virtual IEnumerable<Staff> OurTeam { get; set; }
+    }
+}
