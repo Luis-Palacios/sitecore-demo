948a53e0ef710e18eed3203228a18b8d61fc4b51
Luis Palacios <lrpalaciosdev@gmail.com>
2018-11-12 14:14:30 -0600

    implemet glassmapper

---
+++ SitecoreDemo.Web/Models/DefaultPage.cs
@@ -0,0 +1,8 @@
+﻿namespace SitecoreDemo.Web.Models
+{
+    public class DefaultPage
+    {
+        public virtual string PageTitle { get; set; }
+        public virtual string MetaDescription { get; set; }
+    }
+}
